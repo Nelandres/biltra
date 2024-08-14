b75b07517c0e8e38641303e2e098fc6fce110b0d
NelAndres <nelson.diazrroun.edu.co>
2024-08-14 15:20:23 -0500

    Updates

---
+++ precache-manifest.fb79a52dc4ba3970f1866490ae4cb02a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81e061beb0b84d1561dea1048a7dd4cb",
+    "url": "/biltra/index.html"
+  },
+  {
+    "revision": "1b7d77441527c01d24d5",
+    "url": "/biltra/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c555c3a67cb1e954a33a",
+    "url": "/biltra/static/js/2.a357cf17.chunk.js"
+  },
+  {
+    "revision": "80268ef01b4c0954a06c3d5c20df2edd",
+    "url": "/biltra/static/js/2.a357cf17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b7d77441527c01d24d5",
+    "url": "/biltra/static/js/main.ee201a09.chunk.js"
+  },
+  {
+    "revision": "cb7d5ab679249c8601f9",
+    "url": "/biltra/static/js/runtime-main.a663d4ba.js"
+  }
+]);
