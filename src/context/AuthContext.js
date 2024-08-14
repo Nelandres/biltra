095a4d1ead0f60de28818fdab15b673ed982b5d6
NelAndres <nelson.diazrroun.edu.co>
2024-08-14 08:56:32 -0500

    Initial commit

---
+++ src/context/AuthContext.js
@@ -0,0 +1,23 @@
+// src/context/AuthContext.js
+import React, { createContext, useState, useContext, useEffect } from 'react';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      // Verifica el token y obtiene los datos del usuario
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
